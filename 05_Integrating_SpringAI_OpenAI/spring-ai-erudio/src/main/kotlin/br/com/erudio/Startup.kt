7c807342fa6965668e14cff7e1bff0f32283091e
leandrocgsi <leandrocgsi@gmail.com>
2025-04-26 09:52:32 -0300

    Integrating a Spring Boot Application with Spring AI, OpenAI and ChatGPT

--- 05_Integrating_SpringAI_OpenAI/spring-ai-erudio/src/main/kotlin/br/com/erudio/Startup.kt
+++ 05_Integrating_SpringAI_OpenAI/spring-ai-erudio/src/main/kotlin/br/com/erudio/Startup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Startup
+class SpringAiErudioApplication
 
 fun main(args: Array<String>) {
-    runApplication<Startup>(*args)
+	runApplication<SpringAiErudioApplication>(*args)
 }
