dcaead08cb526c911c4b8eec345596f5d75fb0b3
leandrocgsi <leandrocgsi@gmail.com>
2025-04-26 09:53:13 -0300

    Implementing the React JS Client to Consume the API with Spring AI OpenAI and ChatGPT

--- 07_Integrating_SpringAI_OpenAI_ClientReactJS/api/spring-ai-erudio/src/main/kotlin/br/com/erudio/service/ChatService.kt
+++ 07_Integrating_SpringAI_OpenAI_ClientReactJS/api/spring-ai-erudio/src/main/kotlin/br/com/erudio/service/ChatService.kt
@@ -5,8 +5,10 @@ import org.springframework.ai.chat.prompt.Prompt
 import org.springframework.ai.openai.OpenAiChatOptions
 import org.springframework.stereotype.Service
 
+
 @Service
-class ChatService(private val chatModel: ChatModel) {
+class ChatService (private val chatModel: ChatModel) {
+
     fun getResponse(prompt: String?): String? {
         return chatModel.call(prompt)
     }
